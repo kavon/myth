9d7c1027a852b1d9affb6a202bfe69b1a41d651a
Peter-Michael Osera <psosera@gmail.com>
2015-06-29 12:25:47 -0400

    Shrunk down example sets for programs with largest #/example-to-prog. size ratios.

--- generate-data.py
+++ generate-data.py
@@ -19,28 +19,38 @@ def find_tests(root):
         tests.extend([(path, f[0]) for f in files if f[1] == TEST_EXT])
     return tests
 
-def gather_data(prog, path, base):
-    print(subprocess.check_output([prog] + FLAGS + [join(path, base + TEST_EXT)], stderr=subprocess.STDOUT), end='')
+def parse_data(line):
+    tokens = line.strip().split(',')
+    return { "name" : tokens[0], "examples" : int(tokens[1]), "size" : int(tokens[2]), "time" : float(tokens[3]) }
+
+def print_data(prog, path, base, trials):
+    cmd = [prog] + FLAGS + [join(path, base + TEST_EXT)]
+    data = parse_data(subprocess.check_output(cmd, stderr=subprocess.STDOUT))
+    for i in range(0, trials-1):
+        data["time"] += parse_data(subprocess.check_output(cmd, stderr=subprocess.STDOUT))["time"]
+    data["time"] /= float(trials)
+    print("{0},{1},{2},{3}".format(data["name"], data["examples"], data["size"], data["time"]))
 
 def print_usage(args):
-    print("Usage: {0} <prog> <test|testdir>".format(args[0]))
+    print("Usage: {0} <prog> <test|testdir> <#/trials>".format(args[0]))
 
 def main(args):
-    if len(args) == 3:
+    if len(args) == 4:
         prog = args[1]
         path = args[2]
+        trials = int(args[3])
         if not os.path.exists(prog):
             print_usage(args)
         elif os.path.exists(path) and os.path.isdir(path):
             for path, base in find_tests(path):
-                gather_data(prog, path, base)
+                print_data(prog, path, base, trials)
         else:
             path, filename = os.path.split(path)
             base, ext = splitext(filename)
             if ext != TEST_EXT:
                 print_usage(args)
             else:
-                gather_data(prog, path, base)
+                print_data(prog, path, base, trials)
     else:
         print_usage(args)
 
--- tests/pldi-2015-contexts/tree_nodes_at_level.ml
+++ tests/pldi-2015-contexts/tree_nodes_at_level.ml
@@ -4,36 +4,22 @@ let tree_nodes_at_level : tree -> nat -> nat |>
   { Leaf =>
     ( 0 => 0
     | 1 => 0
-    | 2 => 0
-    | 3 => 0
     )
   | Node (Leaf, True, Leaf) =>
     ( 0 => 1
     | 1 => 0
-    | 2 => 0
-    | 3 => 0
     )
   | Node (Node (Leaf, True, Leaf), True, Leaf) =>
     ( 0 => 1
     | 1 => 1
-    | 2 => 0
-    | 3 => 0
-    )
-  | Node (Leaf, True, Node (Leaf, True, Leaf)) =>
-    ( 0 => 1
-    | 1 => 1
-    | 2 => 0
-    | 3 => 0
     )
   | Node (Node (Leaf, True, Leaf), True, Node (Leaf, True, Leaf)) =>
     ( 0 => 1
     | 1 => 2
     | 2 => 0
-    | 3 => 0
     )
   | Node (Node (Node(Leaf, True, Leaf), True, Node (Leaf, True, Leaf)), True, Leaf) =>
     ( 0 => 1
     | 1 => 1
-    | 2 => 2
-    | 3 => 0)
+    )
   } = ?
